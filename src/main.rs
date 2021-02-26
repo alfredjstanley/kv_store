3fb25f7756477d3375ad3f176afde293b13aca3c
alfredjstanley <alfredjstanley56@gmail.com>
2021-02-25 22:39:09 -0500

    hashmap added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Database {
+        // read the kv.db file
+        // parse the string
+        // populate our map
+        Database {map: HashMap::new()}
+    }
+}
+
 fn main() {
-    println!("Hello, Alfred J. Stanley");
+    let mut args = std::env::args().skip(1);
+    let key = args.next().unwrap();
+    let value = args.next().unwrap();
+
+    let content = format!("{}\t{}\n", key, value);
+
+
+    std::fs::write("kv.db", content).unwrap();
+
+    let database = Database::new();
+
 }
