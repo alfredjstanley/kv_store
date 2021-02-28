25bdbfc936ca815e2bd917f40de72cb1287cbe1e
alfredjstanley <alfredjstanley56@gmail.com>
2021-02-28 06:32:48 -0500

    flush method added

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,54 @@
-use std::collections::HashMap;
-
-struct Database {
-    map: HashMap<String, String>,
-}
-
-impl Database {
-    fn new() -> Database {
-        // read the kv.db file
-        // parse the string
-        // populate our map
-        Database {map: HashMap::new()}
-    }
-}
+use std::{collections::HashMap};
 
 fn main() {
     let mut args = std::env::args().skip(1);
     let key = args.next().unwrap();
     let value = args.next().unwrap();
 
-    let content = format!("{}\t{}\n", key, value);
+    let mut database = Database::new().expect("creating db failed");
+    database.insert(key.to_uppercase(), value.clone());
+    database.insert(key, value);
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database,std::io::Error> {
+        // read the kv.db file
+        let contents = std::fs::read_to_string("kv.db")?;
+
+        let mut map = HashMap::new();
 
 
-    std::fs::write("kv.db", content).unwrap();
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("no key");
+            let value = chunks.next().expect("no value");
+            println!("key is {:#?} and value is {:#?}", key, value);
+            map.insert(key.to_owned(), value.to_owned());
+            
+        }
+        
+        // parse the string
+        // populate our map
+        Ok(Database {map: map})
+    }
 
-    let database = Database::new();
+    fn insert(&mut self, key:String, value: String) {
+        self.map.insert(key, value);
+
+    }
+    fn flush(self) -> Result<(), std::io::Error> {
+        let mut contents = String::new();
+        for pairs in self.map {
+            let kvpair = format!("{}\t{}", pairs.0, pairs.1);
+            contents.push_str(&kvpair);
+        }
+
+        std::fs::write("kv.db", contents)
+    }
+
 
 }
